d9671c630fb2eb2c5a81661de14beb1467bc2959
James Johnson <d0c.s4vage@gmail.com>
2019-11-10 20:35:53 -0800

    More robust cloning and updating

--- bash_init.sh
+++ bash_init.sh
@@ -5,6 +5,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 THIS_PROG="$0"
 
 
+_COLOR_DIM="\e[2m"
+_COLOR_BOLD="\e[1m"
+_COLOR_RED="\e[91m"
+_COLOR_YELLOW="\e[93m"
+_COLOR_GREEN="\e[92m"
+_COLOR_RESET="\e[0m"
+
+
+function _hide {
+    "$@" >/dev/null 2>&1
+}
+
+function _muted_indent {
+    echo -en $_COLOR_DIM
+    cat | sed 's/^/    /'g
+    echo -en $_COLOR_RESET
+}
+
 function superdots-realpath {
     if command -v realpath >/dev/null 2>&1 ; then
         realpath "$@"
@@ -38,7 +56,7 @@ function superdots-echo {
     local level=$1
     shift
 
-    if [ "$level" = "DEBUG" ] && [ $SUPERDOTS_DEBUG = false ] ; then
+    if [ "$level" = "DEBUG" ] && [ "$SUPERDOTS_DEBUG" = false ] ; then
         return 0
     fi
 
@@ -46,19 +64,27 @@ function superdots-echo {
 }
 
 function superdots-debug {
+    echo -en $_COLOR_DIM
     superdots-echo "DEBUG" "$@"
+    echo -en $_COLOR_RESET
 }
 
 function superdots-info {
+    echo -en $_COLOR_BOLD
     superdots-echo " INFO" "$@"
+    echo -en $_COLOR_RESET
 }
 
 function superdots-warn {
+    echo -en $_COLOR_YELLOW
     superdots-echo " WARN" "$@"
+    echo -en $_COLOR_RESET
 }
 
 function superdots-err {
+    echo -en $_COLOR_RED
     superdots-echo "  ERR" "$@"
+    echo -en $_COLOR_RESET
 }
 
 function superdots-ensure-deps {
@@ -123,40 +149,66 @@ function superdots-clone-url {
     fi
 }
 
-function superdots-fetch-dot {
+function superdots-clone-dot {
     superdots-info "Fetching $1"
     local local_path=$(superdots-localname "$1")
     local clone_url=$(superdots-clone-url "$1")
 
     superdots-debug "Cloning $clone_url to $local_path"
+    local clone_log="${SUPERDOTS_LOG}.${local_path}"
 
     local target_dir="${SUPERDOTS}/dots/${local_path}"
+    if [ -d "$target_dir" ] ; then
+        superdots-info "Already installed"
+        return 0
+    fi
+
     git clone \
         "$clone_url" \
         "$target_dir" \
-            >"$SUPERDOTS_LOG" 2>&1
+            >"$clone_log" 2>&1
 
     if [ $? -ne 0 ] ; then
-        superdots-warn "Could not clone $clone_url, see $SUPERDOTS_LOG"
-    else
-        superdots-info "Installed"
+        superdots-warn "Could not clone $clone_url:"
+        superdots-warn "---------------------------"
+        cat "$clone_log" | _muted_indent
+        superdots-warn "---------------------------"
+        superdots-warn "See $clone_log for full output"
+        return 1
     fi
+
+    superdots-info "Installed"
 }
 
 function superdots-update-dot {
     local local_path=$(superdots-localname "$1")
+    local force="$2"
 
     local target_dir="${SUPERDOTS}/dots/${local_path}"
-    if [ ! -e "$target_dir" ] ; then
+    if [ ! -d "$target_dir" ] ; then
         superdots-warn "Declared superdot $1 does has not been installed"
         return 1
     fi
 
+    local clone_log="${SUPERDOTS_LOG}.${local_path}"
+
     superdots-info "Updating $local_path"
     (
-        cd $target_dir
-        git pull origin $(git rev-parse --abbrev-ref HEAD) >"$SUPERDOTS_LOG" 2>&1 
+        cd "$target_dir"
+        if [ "$force" == true ] ; then
+            superdots-warn "forcefully resetting any local changes"
+            git reset --hard HEAD >"$clone_log" 2>&1
+        fi
+        git pull origin $(git rev-parse --abbrev-ref HEAD) >"$clone_log" 2>&1 
     )
+    if [ $? -ne 0 ] ; then
+        superdots-warn "Could not update $1:"
+        superdots-warn "-------------------"
+        cat "$clone_log" | _muted_indent
+        superdots-warn "-------------------"
+        superdots-warn "See $clone_log for full output"
+        return 1
+    fi
     superdots-info "    Done"
 }
 
@@ -177,8 +229,7 @@ function superdots {
 }
 
 function superdots-install {
-    superdots-ensure-deps "${SUPERDOTS_DEPS[@]}"
-    if [ $? -ne 0 ] ; then
+    if ! superdots-ensure-deps "${SUPERDOTS_DEPS[@]}" ; then
         superdots-err "Missing dependencies, bailing installation"
         return 1
     fi
@@ -186,14 +237,16 @@ function superdots-install {
     superdots-debug "Ensuring ${#DOTS_LIST[@]} superdots are installed"
 
     for dot in "${DOTS_LIST[@]}" ; do
-        superdots-fetch-dot "$dot"
+        if ! superdots-clone-dot "$dot" ; then
+            superdots-err "Could not clone superdot plugin $dot"
+            continue
+        fi
         superdots-source-dot "$dot"
     done
 }
 
 function superdots-update {
-    superdots-ensure-deps "${SUPERDOTS_DEPS[@]}"
-    if [ $? -ne 0 ] ; then
+    if ! superdots-ensure-deps "${SUPERDOTS_DEPS[@]}" ; then
         superdots-err "Missing dependencies, bailing installation"
         return 1
     fi
